f568ff51a6db2f5cfe200a275234158a58fefb06
Timo Sand <timo.j.sand@gmail.com>
2014-02-24 09:25:42 +0200

    Moved placement of mongoid version check

--- lib/mongoid-ancestry/version.rb
+++ lib/mongoid-ancestry/version.rb
@@ -1,12 +1,6 @@
 module Mongoid
 
-  def self.mongoid3?
-    ::Mongoid.const_defined? :Observer # deprecated in Mongoid 4.x
-  end
 
-  def self.mongoid2?
-    ::Mongoid.const_defined? :Contexts # deprecated in Mongoid 3.x
-  end
 
   module Ancestry
     VERSION = '0.4.0'
